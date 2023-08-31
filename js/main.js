861ec317f9e3c76d2ea37ce592f25dc615a958b1
Tao Long <tl468@cornell.edu>
2023-08-31 13:24:36 -0400

    nav bar...

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ $(document).ready(function () {
     load_publications();
 
     // update elements on scroll
-    // $(window).scroll(function(){
-    //     var distance = $(window).scrollTop();
-    //     $('.section').each(function (i) {
-    //         if ($(this).position().top <= distance + 250) {
-    //             $('.navbar-nav a').removeClass('active');
-    //             $('.navbar-nav a').eq(i).addClass('active');
-    //         }
-    //     });
-    // });
+    $(window).scroll(function(){
+        var distance = $(window).scrollTop();
+        $('.section').each(function (i) {
+            if ($(this).position().top <= distance + 250) {
+                $('.navbar-nav a').removeClass('active');
+                $('.navbar-nav a').eq(i).addClass('active');
+            }
+        });
+    });
 });
 
 
